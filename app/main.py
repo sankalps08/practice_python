b086715a53827e3bc296a0e4c50f07eec5a840ad
Sankalp <sankalps08@gmail.com>
2024-09-30 11:32:37 +0530

    Added requirment.txtt file

--- app/main.py
+++ app/main.py
@@ -4,9 +4,22 @@ from app.routes import auth
 from .database import engine
 from .routes import posts,users,votes
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
-print(settings.database_username)
+
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
